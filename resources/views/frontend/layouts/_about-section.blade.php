7566b0757f3dc60d68f120b96fa421bc2656a869
Harsh Goplani <harshyaash@gmail.com>
2023-06-07 12:58:14 +0000

    feat/created home page ui

---
+++ resources/views/frontend/layouts/_about-section.blade.php
@@ -0,0 +1,24 @@
+<section id="about" class="about">
+    <div class="container" data-aos="fade-up">
+
+      <div class="row">
+        <div class="col-md-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+          <img src="{{ asset('assets/img/about.jpg') }}" class="img-fluid" alt="" height="500px">
+        </div>
+        <div class="d-flex col-md-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
+            <div class="align-self-center">
+                <h3>CricMania - One stop solution for arranging and scoring your local tournaments.</h3>
+                <p>
+                    Welcome to our CricMania, your ultimate destination for all things cricket! We are passionate about this exhilarating sport and dedicated to providing you with the latest news, updates, and insights from the world of cricket.
+                </p>
+                <ul>
+                  <li><i class="ri-check-double-line"></i> Scoring of Live Matches.</li>
+                  <li><i class="ri-check-double-line"></i> Scheduling Tournaments.</li>
+                  <li><i class="ri-check-double-line"></i> Indvidual Player Stats.</li>
+                </ul>
+              </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
---
+++ resources/views/frontend/layouts/_hero-section.blade.php
@@ -0,0 +1,44 @@
+<section id="hero" class="d-flex align-items-center justify-content-center">
+    <div class="container" data-aos="fade-up">
+
+      <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
+        <div class="col-xl-6 col-lg-8">
+          <h1>Cric<span>Mania</span></h1>
+          <h2>Cricket happens to be an unpredictable game which makes it so interesting.</h2>
+        </div>
+      </div>
+
+      <div class="row gy-4 mt-5 justify-content-center" data-aos="zoom-in" data-aos-delay="250">
+        <div class="col-xl-2 col-md-4">
+        <a href="">
+          <div class="icon-box">
+             <i class="ri-team-fill"></i>
+            <h3>Organizer</h3>
+          </div>
+        </a>
+
+        </div>
+        <div class="col-xl-2 col-md-4">
+        <a href="">
+            <div class="icon-box">
+            <i class="ri-bar-chart-box-line fa fa-bat"></i>
+            <h3>Scorer</h3>
+            {{-- <p>Scorer</p> --}}
+          </div>
+        </a>
+        </div>
+        <div class="col-xl-2 col-md-4">
+        <a href="">
+          <div class="icon-box">
+            <i><span class="material-symbols-outlined">
+                sports_cricket
+                </span></i>
+            <h3>Player</h3>
+
+          </div>
+        </a>
+        </div>
+      </div>
+
+    </div>
+  </section>
